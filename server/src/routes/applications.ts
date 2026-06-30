e0c3aa3c4c1685098d92bc9215091bcf1959ffbc
450186 <146966554+450186@users.noreply.github.com>
2026-06-30 22:43:13 +0100

    Complete CRUD operations for applications api

--- server/src/routes/applications.ts
+++ server/src/routes/applications.ts
@@ -5,8 +5,8 @@ const router = Router();
 
 router.get("/", async(req, res) => {
     try {
-        const response = await pool.query("SELECT * FROM applications");
-        res.json(response.rows);
+        const result = await pool.query("SELECT * FROM applications");
+        res.json(result.rows);
     } catch (err) {
         console.log("Error fetching applications: ", err);
         res.status(500).json({ error: "Error fetching applications" });
@@ -25,17 +25,61 @@ router.post('/', async(req, res) => {
             salary,
             deadline
         } = req.body;
-        const response = await pool.query(
+        const result = await pool.query(
             `INSERT INTO applications (company, role, location, status, job_url, notes, salary, deadline) 
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
             RETURNING *`,
             [company, role, location, status, job_url, notes, salary, deadline]
         );
-        res.status(201).json(response.rows[0]);
+        res.status(201).json(result.rows[0]);
         } catch (err) {
             console.log("Error creating application: ", err);
             res.status(500).json({ error: "Error creating application" });
         }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {
+            company,
+            role, 
+            location,
+            status,
+            job_url,
+            notes,
+            salary,
+            deadline
+        } = req.body;
+        const result = await pool.query(
+            `UPDATE applications 
+            SET company = $1, role = $2, location = $3, status = $4, job_url = $5, notes = $6, salary = $7, deadline = $8
+            WHERE id = $9
+            RETURNING *`,
+            [company, role, location, status, job_url, notes, salary, deadline, id]
+        );
+        if(result.rows.length === 0) {
+            return res.status(404).json({ error: "Application not found" });
+        }
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        console.log("Error updating application: ", err);
+        res.status(500).json({ error: "Error updating application" });
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const result = await pool.query("DELETE FROM applications WHERE id = $1 RETURNING *", [id]);
+        if(result.rows.length === 0) {
+            return res.status(404).json({ error: "Application not found" });
+        }
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        console.log("Error deleting application: ", err);
+        res.status(500).json({ error: "Error deleting application" });
+    }
 })
 
 export default router;
