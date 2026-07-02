2a7ffcd60a964a0a88ba27a99b520c54681a1203
450186 <146966554+450186@users.noreply.github.com>
2026-07-02 15:38:13 +0100

    attempt to fix vite issues

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import { useNavigate, Link } from "react-router-dom"
 import toast from "react-hot-toast"
 import { registerUser } from "../api/auth"
+import { CheckCircle2, Circle } from "lucide-react"
 function Register() {
     const [step, setStep] = useState(1)
     const [username, setUsername] = useState("")
@@ -9,14 +10,23 @@ function Register() {
     const [first_name, setFirstName] = useState("")
     const [last_name, setLastName] = useState("")
     const [email, setEmail] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, [step]);
     const navigate = useNavigate()
 
     const progress = (step / 5) * 100
 
-    const validPassword = 
-        password.length >= 8 &&
-        /[A-Z]/.test(password) &&
-        /\d/.test(password)
+    const hasLength = password.length >= 8;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasNumber = /\d/.test(password);
+
+    const validPassword =
+        hasLength &&
+        hasUppercase &&
+        hasNumber;
 
     const canGoNext = 
         (step === 1 && first_name.trim() !== "") ||
@@ -54,26 +64,55 @@ function Register() {
         <main>
             <form className="register-form" onSubmit={handleRegister}>
                 <h2>Register</h2>
+                {/* <h2>register test</h2> */}
                 {step === 1 && (
-                    <input type="text" value={first_name} onChange={(e) => setFirstName(e.target.value)} placeholder="First Name" />
+                    <input ref={inputRef} type="text" value={first_name} onChange={(e) => setFirstName(e.target.value)} placeholder="First Name" />
                 )}
                 {step === 2 && (
-                    <input type="text" value={last_name} onChange={(e) => setLastName(e.target.value)} placeholder="Last Name" />
+                    <input ref={inputRef} type="text" value={last_name} onChange={(e) => setLastName(e.target.value)} placeholder="Last Name" />
                 )}
                 {step === 3 && (
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+                    <input ref={inputRef} type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
                 )}
                 {step === 4 && (
                     <>
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-                        <p className="password-hint">Password must be at least 8 characters long and contain at least one uppercase letter and one number</p>
+                        <input ref={inputRef} type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                        <ul className="password-reqs">
+                            <li className={hasLength ? "valid" : "invalid"}>
+                                {hasLength ? (
+                                    <CheckCircle2 className="check-icon" style={{color: "#3bdb3e"}} />
+                                ) : (
+                                    <Circle className="check-icon" style={{color: "#2563eb"}} />
+                                )}
+                                <span>8 or more characters</span>
+                            </li>
+                            <li className={hasUppercase ? "valid" : "invalid"}>
+                                {hasUppercase ? (
+                                    <CheckCircle2 className="check-icon" style={{color: "#3bdb3e"}} />
+                                ) : (
+                                    <Circle className="check-icon" style={{color: "#2563eb"}} />
+                                )}
+                                <span>1 or more uppercase letters</span>
+                                </li>
+                            <li className={hasNumber ? "valid" : "invalid"}>
+                                {hasNumber ? (
+                                    <CheckCircle2 className="check-icon" style={{color: "#3bdb3e"}} />
+                                ) : (
+                                    <Circle className="check-icon" style={{color: "#2563eb"}} />
+                                )}    
+                                <span>1 or more numbers</span>
+                            </li>
+                        </ul>
                     </>
                 )}
                 {step === 5 && (
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+                    <input ref={inputRef} type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
                 )}
-                <div className="progress-bar">
-                    <div className="progress-fill" style={{width: `${progress}%`}} />
+                <div className="progress-bar-container">
+                    <div className="progress-text">{step}/5</div>
+                    <div className="progress-bar">
+                        <div className="progress-fill" style={{width: `${progress}%`}} />
+                    </div>
                 </div>
                 <div className="form-actions">
                     {step > 1 ? (
@@ -83,7 +122,7 @@ function Register() {
                     ) : (
                         <div className="button-placeholder" />
                     )}
-
+     
                     {step === 5 ? (
                         <button type="submit" className="register-btn" disabled={!canGoNext}>
                             Register
